f606bb86e5fbc1518a732c1da476c3fefdfbac98
KangSungLee <rkdtjd1020@naver.com>
2024-04-02 17:26:19 +0900

    react

--- App.js
+++ App.js
@@ -15,23 +15,10 @@ const logo ={
 export default function App() {
   return (
     <ScrollView>
-      <Image source={logo} />
-      <Count />
-      <Image source={logo} />
-      <FetchExample />
-      <Image source={logo} />
-      <SectionListBasic />
-      <Image source={logo} />
+
       <FlatListBasic />
-      <Image source={logo} />
-      <PizzaTranslator />
-      <Image source={logo} />
-      <State name='Run Tum Tugger'/>
-      <Image source={logo} />
-      <Cat name='Maru' num='1' />
-      <Cat name='Run Tum Tugger' num='2' />
-      <Image source={logo} />
-      <HellowWorld />
+
+
     </ScrollView>
   );
 }
